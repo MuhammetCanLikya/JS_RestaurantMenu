92a09d69c6ac6dceb97829a3c571260d03381afd
Muhammet Can <96202886+MuhammetCanLikya@users.noreply.github.com>
2023-11-02 21:53:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const menu = [
     id: 2,
     title: "diner double",
     category: "lunch",
-    price: 13.99,
+    price: 93.99,
     img: "./images/item-2.jpeg",
     desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
   },
